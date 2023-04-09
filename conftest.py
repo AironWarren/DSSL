6355f853c1e98ec47ca8e003650aa3b6cf389db7
Airon (Maxim Lysov) <maksim285@mail.ru>
2023-04-09 18:10:30 +0700

    option driver

--- conftest.py
+++ conftest.py
@@ -2,38 +2,72 @@ import allure
 
 import pytest
 
+import os
+from dotenv import load_dotenv
+
 from selene import browser
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 from models.working_with_files import resources_dir
+from utils import attach
 
+
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @allure.step('Open registration form')
 @pytest.fixture(scope="session")
-def open_browser():
-    browser.config.window_height = 1800
-    browser.config.window_width = 1800
-    browser.config.hold_browser_open = True
+def open_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
 
-    options = webdriver.ChromeOptions()
-
-    prefs = {
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        },
         "download.default_directory": resources_dir,
         "download.prompt_for_download": False
     }
 
-    options.add_experimental_option('prefs', prefs)
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options)
 
     browser.config.driver = driver
 
+    browser.config.hold_browser_open = True
     browser.open("https://www.dssl.ru/products/")
 
     yield
 
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
     browser.quit()
 
 
